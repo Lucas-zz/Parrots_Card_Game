57c43e50e3100aecc402fda2c5e674a0cc55eb11
Lucas Azzolini Vieira <lucasazzollinivieira@gmail.com>
2021-11-07 22:13:45 -0300

    timer, flip card function e endGame

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 let quantity = 0;
+let qtdClicks = 0;
+let seconds = 0;
+let idInterval;
+
+function timer() {
+
+    const count = document.querySelector(".seconds")
+    count.innerHTML = ` ${seconds} s`;
+
+    idInterval = setInterval(incrementCounter, 1000);
+}
+
+function incrementCounter() {
+    seconds++;
+
+    const count = document.querySelector(".seconds")
+    count.innerHTML = ` ${seconds} s`;
+}
 
 let imgArray = new Array();
+
 imgArray[0] = new Image();
 imgArray[0].src = '/assets/gif/bobrossparrot.gif';
 imgArray[1] = new Image();
@@ -21,58 +40,93 @@ function startTheGame() {
         quantity = prompt("Com quantas cartas você gostaria de jogar?\robs: entre 4 e 14 cartas, somente nº pares.");
     }
 
-    const board = document.querySelector(".board");
-
-    imgArray.sort(comparador);
+    timer();
 
     let game = [];
 
     for (let i = 0; i < quantity / 2; i++) {
         game.push(imgArray[i]);
         game.push(imgArray[i]);
-
-        game.sort(comparador);
     }
 
+    game.sort(comparator);
+
+    const board = document.querySelector(".board");
     for (let i = 0; i < quantity; i++) {
 
         let source = game[i].outerHTML;
 
         board.innerHTML += `
         <div class="container">
-            <div class="card" onclick="flip(event)" data-identifier="card">
-                <div class="front" data-identifier="front-face">
+            <div class="card" onclick="flip(this)" data-identifier="card">
+                <div class="front-face face no-flip" data-identifier="front-face">
                     <figure><img src="/assets/img/front.png" alt=""></figure>
                 </div>
-                <div class="back" data-identifier="back-face">
+                <div class="back-face face no-flip" data-identifier="back-face">
                     ${source}
                 </div>
             </div>
         </div>
         `
-
     }
 }
 
-function comparador() {
+function comparator() {
     return Math.random() - 0.5;
 }
 
 startTheGame();
 
+function flip(card) {
+    // if (!card.classList.contains(".flip")) {
+    //     card.classList.add(".flip");
+    // }
+    qtdClicks++;
+
+    let front = card.querySelector(".front-face.face");
+    let back = card.querySelector(".back-face.face");
+
+    front.classList.add("flip");
+    back.classList.add("flip");
+
+    front.classList.remove("no-flip");
+    back.classList.remove("no-flip");
+
+    card.removeAttribute("onclick");
+
+    timeOut();
+}
+
+function timeOut() {
+    setTimeout(endGame, 1000);
+}
+
+function endGame() {
+
+    if (document.querySelector(".no-flip") === null) {
+        alert(`Você ganhou em ${qtdClicks} jogadas e ${seconds} segundos`);
+        if (confirm("Você gostaria de reiniciar o jogo?")) {
+            location.reload(true);
+        } else {
+            window.close();
+        }
+    }
+}
+
+
+
+
 // const board = document.querySelector(".board");
 // let source = imgArray[0].outerHTML;
 // board.innerHTML = source;
 
-function flip(event) {
-    let card = event.currentTarget;
+// function flip(event) {
+//     let card = event.currentTarget;
+//     if (card.style.transform == "rotateY(180deg)") {
+//         card.style.transform = "rotateY(0deg)";
+//     }
+//     else {
+//         card.style.transform = "rotateY(180deg)";
+//     }
 
-
-    if (card.style.transform == "rotateY(180deg)") {
-        card.style.transform = "rotateY(0deg)";
-    }
-    else {
-        card.style.transform = "rotateY(180deg)";
-    }
-
-}
+// }
